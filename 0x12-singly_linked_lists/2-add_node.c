98370b1772d9e3bd22e8f979268db9102a7e8ee9
BakaneN <mizbkn@gmail.com>
2023-11-21 13:12:47 -0800

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,7 +19,7 @@ list_t *add_node(list_t **head, const char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		count++;
 	newnode->len = count;
-	newnode->next = head;
+	newnode->next = *head;
 	*head = newnode;
 
 	return (*head);
