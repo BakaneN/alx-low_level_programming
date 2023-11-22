3de99d49088055fa5476eb78efcd67fcdecd5ac8
BakaneN <mizbkn@gmail.com>
2023-11-22 12:00:25 -0800

    0-sum_them_all.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
 * struct dog - dog structure
@@ -8,6 +9,7 @@
 * @age: age of the dog
 * @owner: name of the owner
 */
+
 struct dog
 {
 		char *name;
